6cd7ff16a1ecfe7410c13b04875b7500ea5e1da2
maheenraza1420 <145746422+maheenraza1420@users.noreply.github.com>
2023-10-23 11:29:34 +0530

    Add files via upload

---
+++ PAT303.C
@@ -0,0 +1,18 @@
+void main()
+{
+	int r,sp,c;
+	clrscr();
+	for(r=5;r>=1;r--)
+	{
+	  for(sp=5;sp>=r;sp--)
+	  {
+	    printf(" ");
+	  }
+	  for(c=1;c<=r;c++)
+	  {
+	    printf("%d",r);
+	  }
+	  printf("\n");
+	}
+	getch();
+}
